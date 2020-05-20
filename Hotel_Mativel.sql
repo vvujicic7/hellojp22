9c86d85e1b8ebb03f00d7c1a618259c6b651c0f4
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-05-20 18:13:38 +0200

    Izmjene ne Hotel_Mativel

--- Hotel_Mativel.sql
+++ Hotel_Mativel.sql
@@ -4,48 +4,11 @@ drop database if exists Hotel_Mativel;
 create database Hotel_Mativel;
 use Hotel_Mativel;
 
-create table vlasnik(
-	id 		int not null primary key auto_increment,
-	ime 	varchar (50) not null,
-	prezime varchar (50) not null,
-	email 	varchar (100)not null,
-	oib 	char (11)
-);
-
 create table direktor(
 	id 		int not null primary key auto_increment,
 	ime 	varchar (50) not null,
 	prezime varchar (50) not null,
-	email 	varchar (100) not null,
-	oib 	char (11),
-	vlasnik int not null
-);
-
-create table recepcija(
-	id 				int not null primary key auto_increment,
-	opis 			text,
-	broj_soba 		int not null,
-	vrsta_placanja 	varchar (50) not null,
-	kat 			varchar (50) not null,
-	broj_zaposlenih int
-);
-
-create table soba(
-	id 			int not null primary key auto_increment,
-	vrsta 		varchar (50),
-	cijena 		decimal (18,5) not null,
-	kat 		varchar (50) not null,
-	broj 		varchar (100) not null,
-	direktor 	int not null,
-	recepcija int
-);
-
-create table recepcioner(
-	id 			int not null primary key auto_increment,
-	ime 		varchar (50) not null,
-	prezime 	varchar (50) not null,
-	oib 		char (11) not null,
-	recepcija 	int
+	email 	varchar (100) not null
 );
 
 create table gost(
@@ -54,95 +17,44 @@ create table gost(
 	prezime 		varchar (50) not null,
 	email 			varchar (100) not null,
 	datum_rodjenja 	datetime not null,
-	drzavljanstvo 	char (3) not null,
-	restoran 		int,
-	soba 			int,
-	vozac 			int,
-	vozilo 			int,
-	recepcija 		int
+	drzavljanstvo 	char (3) not null
 );
 
-create table restoran(
+create table djelatnik(
 	id 				int not null primary key auto_increment,
-	vrsta 			varchar (50),
-	opis 			text not null,
-	kat 			varchar (50) not null,
-	cijena_dorucka 	decimal(15,5)
+	ime 			varchar (50) not null,
+	prezime 		varchar (50) not null,
+	oib 			char (11) not null,
+	radno_mjesto 	varchar (50) not null,
+	direktor 		int
 );
 
-create table konobar(
-	id 			int not null primary key auto_increment,
-	ime 		varchar (50) not null,
-	prezime 	varchar (50) not null,
-	staz 		varchar (50),
-	rajom 		varchar (50),
-	restoran 	int
+create table usluga_djelatnika(
+	usluga 		int not null,
+	djelatnik 	int not null
 );
 
-create table vozac(
-	id 			int not null primary key auto_increment,
-	ime 		varchar (50) not null,
-	prezime 	varchar (50) not null,
-	oib 		char (11),
-	recepcija 	int
-);
-
-create table voznje(
-	vozac 	int not null,
-	vozilo 	int not null
-);
-
-create table vozilo(
-	id 			int not null primary key auto_increment,
-	marka 		varchar (50) not null,
-	vrsta 		varchar (50) not null,
-	recepcija 	int
-);
-
-create table prodaja_izleta(
-	recepcioner int not null,
-	izlet 		int not null
-);
-
-create table izlet(
+create table usluga(
 	id 			int not null primary key auto_increment,
 	vrsta 		varchar (50) not null,
-	opis 		text,
 	naziv 		varchar (50) not null,
-	cijena 		decimal (15,5) not null,
-	redni_broj 	varchar (100)
+	opis 		text not null,
+	redni_broj 	varchar (1000) not null
 );
 
-create table gost_izlet(
+create table hotelska_usluga(
 	gost 	int not null,
-	izlet 	int not null
+	usluga 	int not null
 );
+alter table djelatnik add foreign key (direktor) references direktor (id);
 
-#show tables;
-alter table direktor add foreign key (vlasnik) references vlasnik (id);
+alter table usluga_djelatnika add foreign key (djelatnik) references djelatnik (id);
+alter table usluga_djelatnika add foreign key (usluga) references usluga (id);
 
-alter table soba add foreign key (direktor) references direktor (id);
-alter table soba add foreign key (recepcija) references recepcija (id);
+alter table hotelska_usluga add foreign key (usluga) references usluga (id);
+alter table hotelska_usluga add foreign key (gost) references gost (id);
 
-alter table recepcioner add foreign key (recepcija) references recepcija (id);
 
-alter table vozac add foreign key (recepcija) references recepcija (id);
 
-alter table vozilo add foreign key (recepcija) references recepcija (id);
-
-alter table voznje add foreign key (vozac) references vozac (id);
-alter table voznje add foreign key (vozilo) references vozilo (id);
-
-alter table gost add foreign key (soba) references soba (id);
-alter table gost add foreign key (restoran) references restoran (id);
-alter table gost add foreign key (recepcija) references recepcija (id);
-
-alter table konobar add foreign key (restoran) references restoran (id);
-
-alter table prodaja_izleta add foreign key (recepcioner) references recepcioner (id);
-alter table prodaja_izleta add foreign key (izlet) references izlet (id);
-
-alter table gost_izlet add foreign key (gost) references gost (id);
-alter table gost_izlet add foreign key (izlet) references izlet (id);
 
 
